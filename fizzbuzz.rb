9abdf38e9caaccad9c06ef956c30e9fdd8518c7b
Nick Graffagna <n.graffagna@gmail.com>
2018-06-27 16:55:19 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,9 @@ def fizzbuzz(int)
     "Fizz"
   elsif int % 5 == 0
     "Buzz"
-  else (int % 5 == 0) && (int % 3 == 0)
+  elsif (int % 5 == 0) && (int % 3 == 0)
     "FizzBuzz"
+  else (int % 5 != 0) || (int % 3 != 0)
+    "nil"
   end
 end
